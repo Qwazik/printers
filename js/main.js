ac6602726a2d71d7353c50324cb5928793945e27
Qwazik <qwazik21@gmail.com>
2017-08-24 10:41:05 +0300

    update

--- js/main.js
+++ js/main.js
@@ -4,14 +4,36 @@ $(function(){
 	initStageBanner();
 
 	$('.owl-carousel').each(function(){
+		var items = $(this).data('items');
+		var mdItems, smItems = 2, xsItems = 1;
+		(items == 4)?mdItems = 3:(items == 3)?mdItems = 2: null;
+		// (items == 4)?smItems = 2:(items == 3)?smItems = 2: null;
 		$(this).owlCarousel({
-			items: $(this).data('items'),
+			items: items,
 			nav: true,
 			loop: true,
 			autoplay: true,
-			autoplayTimeout: 7000
+			autoplayTimeout: 7000,
+			responsive: {
+				0:{
+					items: xsItems
+				},
+				767:{
+					items: smItems
+				},
+				992:{
+					items: mdItems
+				},
+				1240:{
+					items: items
+				}
+			}
 		});
 	});
+
+	$('.main-photo__thumbs').owlCarousel({
+		items: 4
+	}).addClass('owl-carousel');
 
 
 	//card photo thumbs
@@ -24,11 +46,29 @@ $(function(){
 	});
 	
 	//float menu
-	if($('.left-menu').length){
-		leftMenuInit();
-	}
+	// if($('.left-menu').length){
+	// 	leftMenuInit();
+	// }
 
+	$('.mobile-header div').each(function(){
+		var mobileHeader = $('.mobile-header');
+		var copyId = $(this).data('copy');
+		mobileHeader.append($(copyId).clone());
+	});
 
+	$('.mobile-header').on('click', '.catalog-btn > a', function(){
+		var menu = $(this).siblings('.submenu');
+		menu.toggleClass('open');
+		return false;
+	});
+
+	$(document).click(function(e){
+		var menu = $('.mobile-header .catalog-btn .submenu');
+		var noMenu = $(e.target).closest('.catalog-btn').length;
+		if(menu.is('.open') && !noMenu){
+			menu.removeClass('open');
+		}
+	});
 });
 
 function leftMenuInit(){
